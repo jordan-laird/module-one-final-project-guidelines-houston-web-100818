b1a2d90fa2ebb2e21d7af4924c735d6b3e093267
jordan-laird <jordanmack.laird@gmail.com>
2018-10-23 17:00:47 -0500

    Added user name input

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,27 @@
 require_relative '../config/environment'
 
+def start
+    puts "Hello! Welcome to FilmGrab!"
+    puts "-----------------------------------------"
+end
+
+def get_name
+    puts "Enter your full name."
+    user_input = gets.chomp
+
+    if User.exists?(:name => user_input)
+        puts "Welcome back #{user_input}!"
+    else
+        User.create(:name => user_input)
+        puts "Welcome #{user_input}!"
+    end
+end
+
+def user_options
 
 
-puts "HELLO WORLD"
+end
+start
+get_name
+
+binding.pry
