80a24e740cba6c41adfbbaf83c16d38bef565a45
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-10-25 18:55:41 -0500

    All the things

--- bin/run.rb
+++ bin/run.rb
@@ -15,7 +15,39 @@ found_user = User.find_or_create_by(name: user_input)
     puts "#{found_user.name}"
   end
 
-  @prompt.select("Available Options", %w(My\ Tickets All\ Movies Find\ By\ Rating Find\ By\ Title))
+  return_value = @prompt.select("Available Options", %w(My\ Tickets All\ Movies Find\ By\ Rating Find\ By\ Title))
 
-  binding.pry
+
+
+  if return_value == "My Tickets"
+    if nil
+      puts "You have no tickets"
+    else
+      Ticket.all.select do |t|
+      t.users == found_user
+      end
+    end
+     
+    
+  elsif return_value == "All Movies"
+    Movie.all.each do |movie|
+      puts movie.title 
+    end
+ 
+  elsif return_value == "Find By Rating"
+    rating_input = @prompt.select("Available Options", %w(PG PG-13 R))
+    Movie.all.each do |m|
+      if m.rating == rating_input
+        puts m.title
+      end
+    end
+    
+  
+
+  # elsif return_value == "Find By Title"
+
+  else 
+    puts "Invalid Command"
+  end
+  
   0
