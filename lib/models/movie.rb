ada4febcbf281e6d288f0eda602edc53f0a6dc29
jordan-laird <jordanmack.laird@gmail.com>
2018-10-25 16:38:19 -0500

    Building methods

--- lib/models/movie.rb
+++ lib/models/movie.rb
@@ -2,4 +2,22 @@ class Movie < ActiveRecord::Base
     has_many :tickets
     has_many :users, through: :tickets
 
+
+    def self.all_movie_titles
+        Movie.all.map  do |movie|
+            movie.title
+        end
+    end
+
+    def find_by_title(title)
+        Movie.all.select do |title|
+            title == Movie.title
+        end
+    
+    end
+
+    def find_by_rating(input_rating)
+        Movie.where(rating: input_rating)
+    end
+
 end
