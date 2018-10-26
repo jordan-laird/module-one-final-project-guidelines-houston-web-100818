323a7bd646f54ee288d90c7355b4f4aa2f0b36fd
jordan-laird <jordanmack.laird@gmail.com>
2018-10-26 14:27:15 -0500

    Getting things done

--- bin/run.rb
+++ bin/run.rb
@@ -2,60 +2,91 @@ require_relative '../config/environment'
 require 'rest-client'
 require 'pry'
 
-@prompt = TTY::Prompt.new
-
-@prompt.say("Hello! Welcome to FilmGrab!")
-
-user_input = @prompt.ask('Please enter your name:')
-found_user = User.find_or_create_by(name: user_input)
-
-  if found_user != nil
-    puts "Welcome #{found_user.name}"
-  else
-    puts "#{found_user.name}"
-  end
-
-  return_value = @prompt.select("Available Options", %w(My\ Tickets All\ Movies Find\ By\ Rating Find\ By\ Title))
 
 
-
-  if return_value == "My Tickets"
-    if nil
-      puts "You have no tickets"
-    else
-      Ticket.all.select do |t|
-      t.users == found_user.name
-      end
+  @prompt = TTY::Prompt.new
+  @prompt.say("Hello! Welcome to FilmGrab!")
+  
+  def output_by_rating(all_movies, found_user)
+    x = @prompt.select("Click to see more:") do |options|
+      all_movies.collect do |movie|
+        options.choice movie.title
+      end 
     end
-     
-    
-  elsif return_value == "All Movies"
-    selected_title = @prompt.select("Available Options", Movie.all.map{ |movie| movie.title})
-    selected_movie = Movie.find_by(title: selected_title)
+    selected_movie = Movie.find_by(title: x)
     Ticket.create(user: found_user, movie: selected_movie)
-    binding.pry
-  
-    # Movie.all.each do |movie|
-    #   puts movie.title 
-    # end
- 
-  elsif return_value == "Find By Rating"
-    rating_input = @prompt.select("Available Options", %w(PG PG-13 R))
-    Movie.all.each do |m|
-      if m.rating == rating_input
-        puts m.title
-      end
-    end
-    
+        puts "Thank you"
+
+        start_prompt
+  end 
   
 
-  # elsif return_value == "Find By Title"
 
-  end
-  
-  # if user_input == "exit"
-  #       exit
-  #   elsif user_input == "back"
-  #       start_prompt 
+  # if found != nil
+  #   puts "Welcome #{found.name}"
+  # else
+  #   puts "#{found.name}"
   # end
+  # found
+
+
+  def start_prompt
+    user_input = @prompt.ask('Please enter your name:')
+    found_user = User.find_or_create_by(name: user_input)
+
+    return_value = @prompt.select("Available Options", %w(My\ Tickets All\ Movies Find\ By\ Rating Exit))
+   
+
+    if return_value == "My Tickets"
+        y = []
+        Ticket.all.each do |t|
+          if t.user_id == found_user.id
+          y << Movie.find(t.movie_id).title
+          end
+        end
+        
+
+        if y.empty?
+          puts "You have no movies"
+        else
+          puts y
+        end
+    
+    # if return_value == "My Tickets"
+    #   binding.pry
+    #     Ticket.all.each do |t|
+    #       if t.user_id == found_user.id
+    #         puts t.title
+    #       else
+    #         puts "error"
+    #       end
+    #     end
+      elsif return_value == "All Movies"
+      selected_title = @prompt.select("Available Options", Movie.all.map{ |movie| movie.title})
+      selected_movie = Movie.find_by(title: selected_title)
+      Ticket.create(user: found_user, movie: selected_movie)
+        puts "Thank you"
+        start_prompt
+    
+      # Movie.all.each do |movie|
+      #   puts movie.title 
+      # end
+  
+    elsif return_value == "Find By Rating"
+
+      rating_input = @prompt.select("Available Options", %w(PG PG-13 R))
+      all_by_rating = Movie.all.select do |m|
+        m.rating == rating_input
+      end
+      
+      output_by_rating(all_by_rating, found_user)
+
+     elsif return_value == "Exit"
+      exit
+    end
+  end
+
+  
+  start_prompt
+
   
